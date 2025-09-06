6283066e6eee8ddc44533190076435ef71fcfcbd
alaminislamuchash <alaminislamuchash>
2025-09-06 12:49:04 +0600

    change comit

--- problem_2.js
+++ problem_2.js
@@ -1,12 +1,13 @@
 /* 2.	একটি ফাংশন লেখো যেটা অ্যারের মধ্যে থাকা সংখ্যাগুলোর সর্বোচ্চ মান (max) return করবে  */
-function myfunction(num) {
+function maximum(num) {
   let max = num[0];
   for (let i of num) {
     if (i > max) {
       max = i;
     }
   }
-  console.log("the maximum number is" + max);
+  return max;
 }
-const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
-const result = myfunction(number);
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 200];
+const result = maximum(number);
+console.log(result);
--- problem_4.js
+++ problem_4.js
@@ -1,16 +1,18 @@
 /* 4.	make_avg() ফাংশনটা এমনভাবে পরিবর্তন করো যেন যদি অ্যারে খালি থাকে তাহলে "Array is empty!" return করে।
  */
-function average_number(number) {
+function make_avg(number) {
   let sum = 0;
-  for (let num of number) {
-    sum = sum + num;
+  if (number.length === 0) {
+    const arr = "Array is empty";
+    return arr;
+  } else {
+    for (let num of number) {
+      sum = sum + num;
+    }
+    const average = sum / number.length;
+    return average;
   }
-  const average = sum / alaminarray.length;
-  return average;
-  return sum;
 }
-const alaminarray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
-const alamin = average_number(alaminarray);
-console.log(alamin);
+const value = [];
+const result = make_avg(value);
+console.log(result);
